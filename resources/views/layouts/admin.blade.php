241a377a7968241c7087aeb3f961ab325f970317
nayaaa27 <inayatulhikmahnaya27@gmail.com>
2025-07-06 18:26:41 +0800

    icon button

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <title>Admin Klinik</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://unpkg.com/@tabler/icons-webfont@latest/tabler-icons.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
 </head>
 <body>
 <div class="d-flex">
